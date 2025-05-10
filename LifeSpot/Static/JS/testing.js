a8cd69b80aeb4c4c7593b3026a10871fcfa224e4
IMarunasheva <i.marunasheva@mail.ru>
2025-05-10 23:01:13 +0300

    задания модуля 29

---
+++ LifeSpot/Static/JS/testing.js
@@ -0,0 +1,6 @@
+﻿let userName = prompt("Пожалуйста, введите ваше имя");
+alert(`Приветствуем, ${userName}. В вашем имени ${userName.length} символов`)
+
+let elements = document.getElementsByTagName('*');
+
+alert(`Количество элементов на странице:  ${elements.length}`);
